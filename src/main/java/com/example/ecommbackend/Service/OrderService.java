3f1bb7860319d78c7d3b567d290ff0a4f20c76f3
minazaher <menazaher115@gmail.com>
2023-05-09 17:28:11 +0300

    constraints added

--- src/main/java/com/example/ecommbackend/Service/OrderService.java
+++ src/main/java/com/example/ecommbackend/Service/OrderService.java
@@ -4,6 +4,8 @@ import com.example.ecommbackend.Model.Order;
 import com.example.ecommbackend.Model.Product;
 import com.example.ecommbackend.Repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,32 +15,25 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final ProductService productService;
-    public Order createOrderForUser(int userId, Set<Product> cart){
-        Order order = new Order();
-        int orderPrice = 0;
-        order.setUserId(userId);
 
-        for (Product product : cart) {
-            productService.consumeProduct(product);
-            order.getProducts().add(product);
-            orderPrice += product.getPrice();
-        }
-
-        order.setPrice(orderPrice);
+    public void createOrderForUser(int userId, Set<Product> cart, int price){
+                Order order = Order.builder()
+                .userId(userId)
+                .price(price)
+                .products(cart)
+                .build();
         saveOrder(order);
-        return order;
     }
-    public Order findOrderById(Long id){
+    public Order findOrderById(int id){
         return orderRepository.findOrderById(id);
     }
     public void saveOrder(Order order){
-        orderRepository.save(order);
+            orderRepository.save(order);
     }
     public List<Order> findAllOrders(){
         return orderRepository.findAllWithProducts();
     }
-    public List<Order> findOrderByUserId(Long id) {
+    public List<Order> findOrdersByUserId(int id) {
         return orderRepository.findOrdersByUserId(id);
     }
 }
